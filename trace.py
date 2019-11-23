480f15df71be404352bd98aa79e78f8f20365a15
Ciro S. Costa <cscosta@pivotal.io>
2019-11-23 21:37:41 +0000

    add time tracking
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- trace.py
+++ trace.py
@@ -18,7 +18,7 @@ program = BPF(text=r"""
 
 BPF_PERF_OUTPUT(events);
 
-enum event_type_e
+enum event_type
 {
         EVENT_START = 1,
         EVENT_FINISH,
@@ -27,10 +27,29 @@ enum event_type_e
 struct data_t
 {
         u32 pid, ppid;
+        u64 ts;
         char comm[TASK_COMM_LEN];
         char argv[ARG_LEN];
-        enum event_type_e type;
+        enum event_type type;
 };
+
+
+static inline void
+__submit(struct pt_regs* ctx, enum event_type type)
+{
+        struct task_struct* task;
+        struct data_t data = {};
+
+        task = (struct task_struct*)bpf_get_current_task();
+
+        data.type = type;
+        data.ts = bpf_ktime_get_ns();
+        data.pid = task->tgid;
+        data.ppid = task->real_parent->tgid;
+        bpf_get_current_comm(&data.comm, sizeof(data.comm));
+
+        events.perf_submit(ctx, &data, sizeof(data));
+}
 
 int
 syscall__execve(struct pt_regs* ctx,
@@ -38,19 +57,10 @@ syscall__execve(struct pt_regs* ctx,
                 const char __user* const __user* __argv,
                 const char __user* const __user* __envp)
 {
-        struct task_struct* task;
-        struct data_t data = {};
-
-        task = (struct task_struct*)bpf_get_current_task();
-
-        data.type = EVENT_START;
-        data.pid = task->tgid;
-        data.ppid = task->real_parent->tgid;
-        bpf_get_current_comm(&data.comm, sizeof(data.comm));
-
-        events.perf_submit(ctx, &data, sizeof(data));
-
         bpf_trace_printk("start\n");
+
+        __submit(ctx, EVENT_START);
+
         return 0;
 }
 
@@ -58,6 +68,9 @@ int
 kprobe__do_exit(struct pt_regs* ctx, long code)
 {
         bpf_trace_printk("finish\n");
+
+        __submit(ctx, EVENT_FINISH);
+
         return 0;
 }
 """)
@@ -76,10 +89,24 @@ class EventType(object):
     EVENT_FINISH = 2
 
 
+procs = {}
 
 def handle_events(cpu, data, size):
     event = program["events"].event(data)
-    pprint(event)
+
+    if event.type == EventType.EVENT_START:
+        procs[event.pid] = event
+        print("start")
+
+    elif event.type == EventType.EVENT_FINISH:
+        if not event.pid in procs:
+            return
+
+        seconds_elapsed = (event.ts - procs[event.pid].ts) / (10**9)
+
+        print("finished %s in %3fs" % (event.comm, seconds_elapsed))
+        del procs[event.pid]
+    
 
 
 program["events"].open_perf_buffer(handle_events)
